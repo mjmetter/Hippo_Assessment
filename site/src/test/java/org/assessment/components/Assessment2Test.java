e1fb885e22333c80deb4eb5340369a500cfa0b21
Michael Metternich <michael.j.metternich@gmail.com>
2015-02-25 12:17:56 +0100

    Added assessment assignments as unit tests

---
+++ site/src/test/java/org/assessment/components/Assessment2Test.java
@@ -0,0 +1,63 @@
+package org.assessment.components;
+
+import org.apache.commons.lang.StringUtils;
+import org.hippoecm.repository.HippoRepository;
+import org.hippoecm.repository.HippoRepositoryFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jcr.*;
+
+public class Assessment2Test {
+
+    private Session session;
+
+    @Before
+    public void setUp() throws Exception {
+        String repoUrl="rmi://localhost:1099/hipporepository";
+        String username="admin";
+        char[] password="admin".toCharArray();
+
+        HippoRepository repository = HippoRepositoryFactory.getHippoRepository(repoUrl);
+
+        session = repository.login(username,password);
+    }
+
+    @Test
+    public void traverseContent() throws RepositoryException {
+        Node root = session.getRootNode();
+        iterativeTraverse(0, root);
+    }
+
+    private void iterativeTraverse(final int depth, final Node root) throws RepositoryException {
+        NodeIterator iterator = root.getNodes();
+
+        //depth first iterate through all nodes
+        while (iterator.hasNext()) {
+            Node node = iterator.nextNode();
+            printSingleNode(depth, node);
+            iterativeTraverse( depth + 1, node);
+        }
+    }
+
+    private void printSingleNode(final int numSpaces, final Node node) throws RepositoryException {
+        String start = StringUtils.repeat("-", numSpaces);
+        System.out.println(start + node.getPath());
+
+        PropertyIterator properties = node.getProperties();
+        while (properties.hasNext()) {
+            Property property=properties.nextProperty();
+            if (property.getDefinition().isMultiple()) {
+                System.out.print(start + property.getName() + " = [ ");
+                for (Value value : property.getValues()) {
+                    System.out.print(value.getString() + " ");
+                }
+                System.out.println("]");
+            }
+            else {
+                System.out.println(start + property.getName() + " = " + property.getString());
+            }
+        }
+
+    }
+}
---
+++ site/src/test/java/org/assessment/components/Assessment3Test.java
@@ -0,0 +1,46 @@
+package org.assessment.components;
+
+import org.hippoecm.repository.HippoRepository;
+import org.hippoecm.repository.HippoRepositoryFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jcr.*;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+
+public class Assessment3Test {
+
+    private Session session;
+
+    @Before
+    public void setUp() throws Exception {
+        String repoUrl="rmi://localhost:1099/hipporepository";
+        String username="admin";
+        char[] password="admin".toCharArray();
+
+        HippoRepository repository = HippoRepositoryFactory.getHippoRepository(repoUrl);
+
+        session = repository.login(username,password);
+    }
+
+    @Test
+    public void searchContent() throws RepositoryException {
+//        String xpathQuery = "//*[jcr:contains(.,'fox')]";
+        final String xpathQuery = "//*[jcr:contains(.,'system')]";
+        final QueryManager queryManager = session.getWorkspace().getQueryManager();
+        final Query query = queryManager.createQuery(xpathQuery, "xpath");
+        printResult( query.execute() );
+    }
+
+    private void printResult(final QueryResult result) throws RepositoryException {
+        System.out.println("Results after searching:");
+        NodeIterator iterator = result.getNodes();
+        while(iterator.hasNext()) {
+            Node node = iterator.nextNode();
+            System.out.println(" - Node location: " + node.getPath());
+        }
+    }
+
+}
